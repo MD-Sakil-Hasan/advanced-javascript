2e960422bbcf7afd2661e11cb84394e5ec28d04d
MD-Sakil-Hasan <mdsakilhasan833@gmail.comm>
2020-03-04 02:59:48 +0600

    object

---
+++ double=-vs-triple=.js
@@ -0,0 +1,23 @@
+const first = 2;
+const second = "2";
+
+//double == check only value
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+
+
+//triple === check value and type
+
+if(first === second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+//TRIPLE === is recommended
