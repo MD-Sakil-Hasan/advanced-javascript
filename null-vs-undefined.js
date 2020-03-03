7c5d6a64b16662510ada7ab51cb83c4bbd9ca847
MD-Sakil-Hasan <mdsakilhasan833@gmail.comm>
2020-03-04 01:02:07 +0600

    null vs undefined for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,32 @@
+let bird;
+console.log(bird);
+
+
+function add(num1, num2){
+    console.log(num1, num2);
+    //if you don't return here anything than the value will be undefined
+}
+
+
+function add(num1, num2){
+    console.log(num2);
+}
+
+
+const lover = {name:"smart dude", phone:456576};
+console.log(lover.school);
+
+
+let fun = undefined;
+
+
+
+
+
+
+
+
+
+// null-------------
+
+//null is applied by the programmer
